9c695e1c798457bd4007988ece4a72338e820e05
Nacho Garcia <nash.makineta@gmail.com>
2016-01-10 00:26:18 +0000

    Done with tables. Now I commit because I will change things for submission.

---
+++ public_html/JavaScript/datingws.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var imgpreview = document.getElementById("imgPreview");
+function showPreview() {
+    //imgpreview.innerHTML = '<img src="' + this.value + '"/>';
+}
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        //alert(e.target.result);
+        imgpreview.setAttribute("src", e.target.result);
+        imgpreview.setAttribute("width", 150);
+        imgpreview.setAttribute("height", 200);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
